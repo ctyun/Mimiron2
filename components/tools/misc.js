01e540c772be6c8305164fff5948baf34b5179e9
WangYG <wangyonggang@ctyun.cn>
2016-07-26 08:50:19 +0800

    1.0.6 增加Misc.formatXML方法

--- components/tools/misc.js
+++ components/tools/misc.js
@@ -108,6 +108,33 @@ let Misc={
 		}
 		let hash = window.location.hash.split("/");
 		return hash[hash.length + Number(index)];
+	},
+	formatXML(xml){
+		let formatted = '';
+		let reg = /(>)(<)(\/*)/g;
+		xml = xml.replace(reg, '$1\r\n$2$3');
+		let pad = 0;
+		xml.split('\r\n').forEach((node,index)=>{
+			let indent = 0;
+			if (node.match( /.+<\/\w[^>]*>$/ )) {
+	            indent = 0;
+	        } else if (node.match( /^<\/\w/ )) {
+	            if (pad != 0) {
+	                pad -= 1;
+	            }
+	        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+	            indent = 1;
+	        } else {
+	            indent = 0;
+	        }
+	        let padding = '';
+	        for (let i = 0; i < pad; i++) {
+	            padding += '  ';
+	        }
+	        formatted += padding + node + '\r\n';
+	        pad += indent;
+		});
+		return formatted;
 	}
 }
 
